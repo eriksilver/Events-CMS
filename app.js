6dfc0e4af9b8118200f09c5dd0ad8801b426d8f5
eriksilver <eriksilver@users.noreply.github.com>
2015-11-30 20:14:17 -0600

    added and wired up app.js and routes.js

---
+++ app.js
@@ -0,0 +1,10 @@
+// 'use strict';
+
+var myApp = angular.module('EventCMS', [
+    'ui.router',
+    'firebase',
+]);
+
+myApp.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    //     $logProvider.debugEnabled(true);
+});
---
+++ routes.js
@@ -0,0 +1,23 @@
+'use strict';
+console.log("routes.js declared");
+
+angular.module('EventCMS')
+
+.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    $locationProvider.html5Mode({
+        enabled: true,
+    });
+    // For any unmatched url, send to /
+    // $urlRouterProvider.otherwise('/');
+    $stateProvider
+    .state('eventList', {
+        url: '/',
+        templateUrl: 'listview.html',
+        controller: 'ListCtrl',
+    })
+    .state('addEvent', {
+        url: '/add',
+        templateUrl: 'addevent.html',
+        controller: 'AddCtrl',
+    })
+});
