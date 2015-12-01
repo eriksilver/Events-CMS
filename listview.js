1d7444a5084f146ab8931869a122c1dba6609eb7
eriksilver <eriksilver@users.noreply.github.com>
2015-11-30 20:15:53 -0600

    added listview route with js and html files

---
+++ listview.js
@@ -0,0 +1,29 @@
+angular.module('EventCMS')
+
+.controller("ListCtrl", [
+    "$scope", "$state", "$log","$firebaseArray",
+    function($scope, $state, $log, $firebaseArray) {
+
+        $log.info("ListCtrl ran");
+
+        //create Firebase events array
+        var ref = new Firebase("https://dazzling-torch-1941.firebaseio.com/events");
+
+        // create a synchronized array for events
+        $scope.events = $firebaseArray(ref);
+
+        // setup database schema
+        // the event is automatically added to our Firebase database
+        $scope.addEvent = function() {
+            $scope.events.$add({
+                eventTitle: " ",
+                startDate: " ",
+                endDate: " ",
+                category: " ",
+                description: " ",
+                featuredFlag: " ",
+                createdAt: Firebase.ServerValue.TIMESTAMP,
+                updatedAt: " "
+            });
+        };
+    }]);
