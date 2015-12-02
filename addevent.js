aeda3a044fa67d5d3cf7225c329ac21abff7405a
eriksilver <eriksilver@users.noreply.github.com>
2015-12-02 14:35:36 -0600

    added alerts manager service to handle for success-fail messages

--- addevent.js
+++ addevent.js
@@ -1,23 +1,26 @@
 angular.module('EventCMS')
 
 .controller("AddCtrl", [
-    "$scope", "$state", "$log","$firebaseArray",
-    function($scope, $state, $log, $firebaseArray) {
+    "$scope", "$state", "$log","$firebaseArray", "alertsManager", "$rootScope",
+    function($scope, $state, $log, $firebaseArray, alertsManager, $rootScope) {
 
         $log.info("AddCtrl ran");
 
         //Firebase callback to register sync fail/success
         var onComplete = function(error) {
             if (error) {
-                console.log('Synchronization failed');
+                $rootScope.$broadcast('saveEvent', {message: "Profile Update Failed", result: "failure"});
+                $log.log('Synchronization failed');
             } else {
-                console.log('Synchronization succeeded');
+                $rootScope.$broadcast('saveEvent', {message: "Profile Updated", result: "success"});
+                $log.log('Synchronization succeeded');
             }
         };
 
         //create Firebase events array
         var ref = new Firebase("https://dazzling-torch-1941.firebaseio.com/events");
 
+        //define empty event/ database schema
         var EMPTY_EVENT = {
             eventTitle: " ",
             startDate: new Date(),
@@ -38,18 +41,16 @@ angular.module('EventCMS')
         var unixCurrent = " ";
 
         function transform_dates(start,end) {
-            $log.info("start",start);
+            //convert data picker dates to integer format that is acceptable to Firebase
             unixStart = start.getTime();
             unixEnd = end.getTime();
-
+            //capture current date in unix/integer format
             var d = new Date ();
             unixCurrent = d.getTime();
         };
 
-        // setup database schema
-        // the event is automatically added to our Firebase database
+        //addEvent function will add a newEvent based on database schema
         $scope.addEvent = function() {
-            // var cleandEvent = transform_dates($scope.newEvent);
             transform_dates($scope.newEvent.startDate,$scope.newEvent.endDate);
 
             $scope.newEvent = {
@@ -66,9 +67,30 @@ angular.module('EventCMS')
             //Firebase push method to save newEvent data to array
             ref.push($scope.newEvent,onComplete);
 
+            //call setup empty state to reset the entry form after new event is added
             setup_empty_event_state();
         };
 
+        //call setup empty state to reset the entry form
         setup_empty_event_state();
+
+        //setup alerts in controller scope
+        $scope.alerts = alertsManager.alerts;
+
+        //use $on method to receive Broadcast of Alert message
+        //add the Broadcast message (message, result) to alertsManager
+        $scope.$on('saveEvent', function(event,args) {
+            alertsManager.add({
+                message: args.message,
+                type: args.result,
+                // timeout: 6000,
+            });
+            $scope.$apply();
+        });
+
+        //close alert when icon is clicked by removing array item
+        $scope.closeAlert = function (index) {
+            alertsManager.remove(index);
+        };
 
     }]);
--- app.js
+++ app.js
@@ -8,3 +8,23 @@ var myApp = angular.module('EventCMS', [
 myApp.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
     //     $logProvider.debugEnabled(true);
 });
+
+myApp.factory("alertsManager", ["$log",
+    function($log) {
+        //create array of alerts, referenced by alertsManager.alerts
+        var alerts = [];
+
+        function add_alert(alert) {
+            alerts.push(alert);
+        }
+
+        function remove_alert(index) {
+            alerts.splice(index, 1);
+        }
+        return {
+                alerts: alerts,
+                add: add_alert,
+                remove: remove_alert,
+        };
+    }
+])
