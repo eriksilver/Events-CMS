19aabab16839e60a6c15b651882ffe689ad6cf2e
eriksilver <eriksilver@users.noreply.github.com>
2015-12-02 09:43:41 -0600

    implemented Firebase push method to sync events which includes a suuccess callback

--- addevent.js
+++ addevent.js
@@ -6,14 +6,17 @@ angular.module('EventCMS')
 
         $log.info("AddCtrl ran");
 
-
+        //Firebase callback to register sync fail/success
+        var onComplete = function(error) {
+            if (error) {
+                console.log('Synchronization failed');
+            } else {
+                console.log('Synchronization succeeded');
+            }
+        };
 
         //create Firebase events array
         var ref = new Firebase("https://dazzling-torch-1941.firebaseio.com/events");
-
-
-        // create a synchronized array for events
-        $scope.events = $firebaseArray(ref);
 
         var EMPTY_EVENT = {
             eventTitle: " ",
@@ -48,8 +51,6 @@ angular.module('EventCMS')
         $scope.addEvent = function() {
             // var cleandEvent = transform_dates($scope.newEvent);
             transform_dates($scope.newEvent.startDate,$scope.newEvent.endDate);
-            // var test = $scope.newEvent;
-            // $log.info("test=$scope.newEvent",test);
 
             $scope.newEvent = {
                 eventTitle: $scope.newEvent.eventTitle,
@@ -62,21 +63,11 @@ angular.module('EventCMS')
                 updatedAt: " "
             };
 
-
-            $scope.events.$add($scope.newEvent);
-
-            //use push or set method instead of $add to get access to callback??
-
-            // console.log('oncomplete:',onComplete);
-
-
-
+            //Firebase push method to save newEvent data to array
+            ref.push($scope.newEvent,onComplete);
 
             setup_empty_event_state();
         };
-        // $scope.events.$watch(function(event) {
-        //     console.log("event:",event);
-        // });
 
         setup_empty_event_state();
 
