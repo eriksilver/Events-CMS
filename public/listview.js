64f7cf41f87b5ee486b9391bc78b67048fce8ab6
eriksilver <eriksilver@users.noreply.github.com>
2016-01-19 16:10:41 -0600

    updated log command for scope.events

--- public/listview.js
+++ public/listview.js
@@ -10,6 +10,7 @@ angular.module('EventCMS')
 
         // create a synchronized array for events
         $scope.events = $firebaseArray(ref);
-        console.log("scope.events",$scope.events);
+
+        $log.log("scope.events",$scope.events);
 
 }]);
