fb7019b7ae6be47f159664baee488a80beac522d
eriksilver <eriksilver@users.noreply.github.com>
2016-01-18 20:23:12 -0600

    adding files and dependencies for basic test - final step needed to use AngularFire as a dependency

---
+++ test/appSpec.js
@@ -0,0 +1,100 @@
+
+// angular.module('EventCMS')
+
+describe('controller test', function(){
+    var scope; //we'll use this scope in our tests
+
+    //mock Application to allow us to inject our own dependencies
+    beforeEach(module('EventCMS'));
+    //mock the controller for the same reason and include $rootScope and $controller
+    beforeEach(inject(function($rootScope, $controller){
+        //create an empty scope
+        scope = $rootScope.$new();
+        //declare the controller and inject our empty scope
+        $controller('AppCtrl', {$scope: scope});
+    }));
+    // tests start here
+    it('should have variable text = "Hello World!"', function(){
+        expect(scope.text).toBe('Hello World!');
+    });
+});
+
+
+// describe('Hello World example', function() {
+//
+//      beforeEach(module('EventCMS'));
+//
+//     var HelloWorldController,
+//     scope;
+//
+//     beforeEach(inject(function ($rootScope, $controller) {
+//         scope = $rootScope.$new();
+//         HelloWorldController = $controller('HelloWorldController', {
+//             $scope: scope
+//         });
+//     }));
+//     it('says hello world!', function () {
+//         expect(scope.greeting).toEqual("Hello world!");
+//     });
+//
+// });
+
+
+
+
+// describe("hello world", function () {
+//     var appCtrl;
+//     beforeEach(module('EventCMS'));
+//     beforeEach(inject(function ($controller) {
+//         // appCtrl = $controller("AppCtrl", { $scope: $scope });
+//         appCtrl = $controller("AppCtrl");
+//
+//     }))
+//
+//     describe("controller in app.js", function () {
+//         it("should have a message of hello", function () {
+//             var $scope = {};
+//             // var controller = $controller('AppCtrl', { $scope: $scope });
+//             // expect($scope.message).toEqual("Hello");
+//             expect(appCtrl.message).toBe("Hello");
+//         })
+//     })
+// })
+
+
+// describe("hello world", function () {
+//     beforeEach(module('EventCMS'));
+//
+//     var $controller;
+//
+//     beforeEach(inject(function (_$controller_) {
+//         $controller = _$controller_;
+//     }));
+//
+//     describe("controller in App.js", function () {
+//         it("should have a message of hello", function () {
+//             var $scope = {};
+//             var controller = $controller('AppCtrl', { $scope: $scope });
+//             expect($scope.message).toEqual("Hello");
+//         })
+//     })
+// })
+
+
+// describe("hello world", function () {
+//
+//     beforeEach(module('EventCMS'));
+//
+//     var $controller;
+//
+//     beforeEach(inject(function (_$controller_) {
+//         $controller = _$controller_;
+//     }));
+//
+//     describe("controller in App.js", function () {
+//         it("should have a message of hello", function () {
+//             var controller = $controller('AppCtrl');
+//             expect(controller.message).toBe("Hello")
+//         })
+//     })
+// })
